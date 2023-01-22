fbf644eb362a99ca72706a1711b9598394ae6238
nathanvy <nathan.vany@protonmail.ch>
2023-01-22 13:43:26 -0800

    bleh

--- early-init.el
+++ early-init.el
@@ -2,7 +2,7 @@
 (setq package-enable-at-startup nil)
 
 ;;https://emacs-lsp.github.io/lsp-mode/page/performance/
-(setq gc-cons-threshold (* 1024 1024 100))
+(setq gc-cons-threshold (* 1024 1024 32))
 (setq read-process-output-max (* 1024 1024))
 
 ;;resizing the frame is very expensive, so we don't do it for fonts larger than system default
